b46f717be2d9bcce59ed94a1f97809edc5b7c6cb
No5972 <wujiuqier@foxmail.com>
2020-07-18 00:23:54 +0800

    update

--- deployEx.cmd
+++ deployEx.cmd
@@ -1,8 +1,10 @@
 git add .
+git pull origin master
 git commit -m "update"
 git push origin -u master
 cd themes/next
 git add .
+git pull origin master
 git commit -m "update"
 git push origin -u master
 cd ..
